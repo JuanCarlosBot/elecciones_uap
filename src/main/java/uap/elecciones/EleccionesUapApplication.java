37769319f2f814764cea23633c444a6393ed076c
JuanCarlosBot <yhon.1yhon@gmail.com>
2024-04-09 10:58:12 -0400

    primerCambio

---
+++ src/main/java/uap/elecciones/EleccionesUapApplication.java
@@ -0,0 +1,13 @@
+package uap.elecciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EleccionesUapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EleccionesUapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uap/elecciones/EleccionesUapApplicationTests.java
@@ -0,0 +1,13 @@
+package uap.elecciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EleccionesUapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
