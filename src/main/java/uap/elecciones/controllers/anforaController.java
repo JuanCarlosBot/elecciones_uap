327a347f45439ccf10cf0cfa0db65340170db765
marcocuellar7293@gmail.com <marcocuellar7293@gmail.com>
2024-04-16 15:58:17 -0400

    asdasd

--- src/main/java/uap/elecciones/controllers/anforaController.java
+++ src/main/java/uap/elecciones/controllers/anforaController.java
@@ -34,7 +34,7 @@ import uap.elecciones.model.service.IVotoTotalFrenteService;
 @Controller
 @RequestMapping(value = "/admin")
 public class anforaController {
-    
+
     @Autowired
     private IAnforaService anforaService;
 
@@ -56,10 +56,10 @@ public class anforaController {
     @Autowired
     private IVotoTotalFrenteService votoTotalFrenteService;
 
-       @RequestMapping(value = "/anfora",method = RequestMethod.GET)
-    public String Vista_Anfora(Model model,RedirectAttributes flash, HttpServletRequest request,@RequestParam(name = "succes",required = false)String succes){
+    @RequestMapping(value = "/anfora", method = RequestMethod.GET)
+    public String Vista_Anfora(Model model, RedirectAttributes flash, HttpServletRequest request,
+            @RequestParam(name = "succes", required = false) String succes) {
         if (request.getSession().getAttribute("usuario") != null) {
-            
 
             if (succes != null) {
                 model.addAttribute("succes", succes);
@@ -70,35 +70,31 @@ public class anforaController {
             model.addAttribute("anfora", new Anfora());
             model.addAttribute("mesas", mesas);
             model.addAttribute("niveles", niveles);
-            
+
             return "Anfora/anfora_vista";
         } else {
             return "redirect:/login";
         }
     }
 
-    
-    @RequestMapping(value = "/anfora_form",method = RequestMethod.POST)
+    @RequestMapping(value = "/anfora_form", method = RequestMethod.POST)
     private String Form_Anfora(Model model,
-    @RequestParam("mesa") Long id_mesa,
-    @RequestParam("nivel") Long id_nivel,
-    @RequestParam("cant_voto_nulo") int cant_voto_nulo,
-    @RequestParam("cant_voto_blanco") int cant_voto_blanco,
-    @RequestParam("cant_voto_valido") int cant_voto_valido,
-    RedirectAttributes flash, HttpServletRequest request){
+            @RequestParam("mesa") Long id_mesa,
+            @RequestParam("nivel") Long id_nivel,
+            @RequestParam("cant_voto_nulo") int cant_voto_nulo,
+            @RequestParam("cant_voto_blanco") int cant_voto_blanco,
+            @RequestParam("cant_voto_valido") int cant_voto_valido,
+            RedirectAttributes flash, HttpServletRequest request) {
 
         if (request.getSession().getAttribute("usuario") != null) {
-            
+
             List<Object[]> frentes = anforaService.getDatosDeMesaYFrenteYNivelPorIdMesaYNivel(id_mesa, id_nivel);
-        
-            
+
             model.addAttribute("frentes", frentes);
             model.addAttribute("mesaId", id_mesa);
             model.addAttribute("cant_voto_nulo", cant_voto_nulo);
             model.addAttribute("cant_voto_blanco", cant_voto_blanco);
             model.addAttribute("cant_voto_valido", cant_voto_valido);
-
-         
 
             return "Anfora/anfora_vista_frentes";
         } else {
@@ -106,22 +102,19 @@ public class anforaController {
         }
     }
 
- 
-
-    @RequestMapping(value = "/anfora_frente",method = RequestMethod.POST)
-    public String Vista_Anfora_frente(Model model,RedirectAttributes flash, HttpServletRequest request,@RequestParam(name = "succes",required = false)String succes,
-    @RequestParam(name = "frenteId")Long frenteId,
-    @RequestParam(name = "mesaId")Long mesaId,
-    @RequestParam("cant_voto_nulo") int cant_voto_nulo,
-    @RequestParam("cant_voto_blanco") int cant_voto_blanco,
-    @RequestParam("cant_voto_valido") int cant_voto_valido){
+    @RequestMapping(value = "/anfora_frente", method = RequestMethod.POST)
+    public String Vista_Anfora_frente(Model model, RedirectAttributes flash, HttpServletRequest request,
+            @RequestParam(name = "succes", required = false) String succes,
+            @RequestParam(name = "mesaId") Long mesaId,
+            @RequestParam("cant_voto_nulo") int cant_voto_nulo,
+            @RequestParam("cant_voto_blanco") int cant_voto_blanco,
+            @RequestParam("cant_voto_valido") int cant_voto_valido) {
         if (request.getSession().getAttribute("usuario") != null) {
-            
 
             if (succes != null) {
                 model.addAttribute("succes", succes);
             }
-           
+
             Mesa mesa = mesaService.findOne(mesaId);
 
             Object facultadObject = anforaService.mesaPorFacultad(mesaId);
@@ -130,32 +123,36 @@ public class anforaController {
             ConteoTotal conteoTotal = new ConteoTotal();
 
             if (listConteo.size() == 0) {
-            
-            conteoTotal.setBlanco_total(cant_voto_blanco);
-            conteoTotal.setNulo_total(cant_voto_nulo);
-            conteoTotal.setVoto_valido_total(cant_voto_valido);
-            conteoTotal.setFacultad(nombreFacultad);
-            conteoTotalService.save(conteoTotal);
 
-            }else {
+                conteoTotal.setBlanco_total(cant_voto_blanco);
+                conteoTotal.setNulo_total(cant_voto_nulo);
+                conteoTotal.setVoto_valido_total(cant_voto_valido);
+                conteoTotal.setFacultad(nombreFacultad);
+                conteoTotalService.save(conteoTotal);
+                System.out.println("entro en size 0");
+            } else {
 
-					for (ConteoTotal conteoTotal2 : listConteo) {
-						if (nombreFacultad.equals(conteoTotal2.getFacultad())) {
-							conteoTotal = conteoTotal2;
-						}
-					}
-					for (ConteoTotal conteoTotal2 : listConteo) {
+                for (ConteoTotal conteoTotal2 : listConteo) {
+                    if (nombreFacultad.equals(conteoTotal2.getFacultad())) {
+                        conteoTotal = conteoTotal2;
+                        conteoTotal.setBlanco_total(conteoTotal.getBlanco_total() + cant_voto_blanco);
+                        conteoTotal.setNulo_total(conteoTotal.getNulo_total() + cant_voto_nulo);
+                        conteoTotal.setVoto_valido_total(conteoTotal.getVoto_valido_total() + cant_voto_valido);
+                        conteoTotalService.save(conteoTotal);
+                    }
+                }
+                for (ConteoTotal conteoTotal2 : listConteo) {
 
-						if (!nombreFacultad.equals(conteoTotal2.getFacultad())) {
-                            conteoTotal.setBlanco_total(conteoTotal.getBlanco_total() + cant_voto_blanco);
-                            conteoTotal.setNulo_total(conteoTotal.getNulo_total() + cant_voto_nulo);
-                            conteoTotal.setVoto_valido_total(conteoTotal.getVoto_valido_total() + cant_voto_valido);
-                            conteoTotalService.save(conteoTotal);
-						}
-					}
+                    if (!nombreFacultad.equals(conteoTotal2.getFacultad())) {
+                        conteoTotal.setBlanco_total(conteoTotal.getBlanco_total() + cant_voto_blanco);
+                        conteoTotal.setNulo_total(conteoTotal.getNulo_total() + cant_voto_nulo);
+                        conteoTotal.setVoto_valido_total(conteoTotal.getVoto_valido_total() + cant_voto_valido);
+                        conteoTotalService.save(conteoTotal);
+                        System.out.println("No entro en el igual");
+                    }
+                }
 
-				}
-           
+            }
 
             Anfora anfora = new Anfora();
             anfora.setCant_voto_blanco(cant_voto_blanco);
@@ -164,41 +161,31 @@ public class anforaController {
             anfora.setMesa(mesa);
             anfora.setConteo_total(conteoTotal);
             anforaService.save(anfora);
-        
-            
 
-              // Iterar sobre los parámetros de la solicitud
-        Enumeration<String> parameterNames = request.getParameterNames();
-        while (parameterNames.hasMoreElements()) {
-            String paramName = parameterNames.nextElement();
-            if (paramName.startsWith("cantidadVotantes_")) {
-                // Obtener el valor del parámetro
-                String paramValue = request.getParameter(paramName);
-
-                // Extraer el identificador del frente del nombre del parámetro (ya no es necesario)
-                // Long frenteId = Long.parseLong(frente[3].toString()); // Eliminado
-
-                // Obtener el frente usando el frenteId proporcionado
-                Frente frente = frenteService.findOne(frenteId);
-
-                // Crear un nuevo DetalleAnfora y establecer sus valores
-                DetalleAnfora detalleAnfora = new DetalleAnfora();
-                detalleAnfora.setAnfora(anfora);
-                detalleAnfora.setFrente(frente);
-
+            // Iterar sobre los parámetros de la solicitud
+            Enumeration<String> parameterNames = request.getParameterNames();
+            while (parameterNames.hasMoreElements()) {
+                String paramName = parameterNames.nextElement();
                 if (paramName.startsWith("cantidadVotantes_")) {
+                    // Obtener el valor del parámetro
+                    String paramValue = request.getParameter(paramName);
+
+                    // Obtener el frenteId a partir del paramName
+                    Long frenteId = Long.parseLong(paramName.substring("cantidadVotantes_".length()));
+
+                    // Obtener el frente usando el frenteId proporcionado
+                    Frente frente = frenteService.findOne(frenteId);
+
+                    // Crear un nuevo DetalleAnfora y establecer sus valores
+                    DetalleAnfora detalleAnfora = new DetalleAnfora();
+                    detalleAnfora.setAnfora(anfora);
+                    detalleAnfora.setFrente(frente);
                     detalleAnfora.setCant_votantes(Integer.parseInt(paramValue));
+
+                    // Guardar el DetalleAnfora en la base de datos
+                    detalleAnforaService.save(detalleAnfora);
                 }
-                
-
-
-
-
-
-                // Guardar el DetalleAnfora en la base de datos
-                detalleAnforaService.save(detalleAnfora);
             }
-        }
 
             return "redirect:/admin/inicio";
         } else {
