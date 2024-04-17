865e93c73c80265a3b365d9bc7cc9b6ac138d5af
abrahan24 <asaraviacoca@gmail.com>
2024-04-17 13:52:41 -0400

    UPDATE 17-04-2024

--- src/main/java/uap/elecciones/controllers/IndexController.java
+++ src/main/java/uap/elecciones/controllers/IndexController.java
@@ -1,12 +1,16 @@
 package uap.elecciones.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.expression.ParseException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import uap.elecciones.model.service.IAsignacionHabilitadoService;
 
@@ -30,25 +34,36 @@ public class IndexController {
     }
 
     @RequestMapping(value = "/estudiante",method = RequestMethod.GET)
-    public String vista_estudiantes(Model model){
+    public String vista_estudiantes(Model model, @RequestParam(name = "succes",required = false)String succes){
+
+        if (succes != null) {
+            model.addAttribute("succes", succes);
+        }
 
         return "Publico/vista_publica_estudiantes";
     }
 
     @RequestMapping(value = "/mesa_asignada/{ru}",method = RequestMethod.GET)
-    public String mesa_asignada_form(Model model,@PathVariable(name = "ru",required = false)String ru){
+    public String mesa_asignada_form(Model model,@PathVariable(name = "ru",required = false)String ru, RedirectAttributes flash ){
 
         
         try {
             if (ru != null) {
-                model.addAttribute("estudiante", asignacionHabilitadoService.asignado_habilitado(ru));
+                
+                if (asignacionHabilitadoService.asignado_habilitado(ru) != null) {
+                    model.addAttribute("estudiante", asignacionHabilitadoService.asignado_habilitado(ru));
+                    return "Content/content :: content";
+                }else{
+                    model.addAttribute("succes", "Estudiante No Matriculado");
+                    return "Content/content :: alert_modal";
+                }
             }
             return "Content/content :: content";
-        } catch (Exception e) {
-            System.out.println("Error");
-            
+        }  catch (Exception e) {
+            System.out.println("6");
+            flash.addFlashAttribute("succes", "ERROR!");
+            return "redirect:/estudiante";
         }
-        return "redirect:/estudiante";
     }
 
 }
