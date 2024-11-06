a792abdacb6f636d7836693733b92eaa7b5e2073
jhowillneytan <jhowillneytan@gmail.com>
2024-11-06 17:50:48 -0400

    exportar todo en excel

--- src/main/java/uap/elecciones/controllers/delegadoController.java
+++ src/main/java/uap/elecciones/controllers/delegadoController.java
@@ -1,9 +1,27 @@
 package uap.elecciones.controllers;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -47,7 +65,6 @@ public class delegadoController {
     @GetMapping("/ventana")
     public String ventana(Model model, HttpServletRequest request) {
 
-        
         if (request.getSession().getAttribute("admin") != null) {
             HttpSession session = request.getSession(false);
             session.invalidate();
@@ -116,26 +133,27 @@ public class delegadoController {
     }
 
     // @GetMapping("/tablaDelegadosGeneral/{idMesa}")
-    // public ResponseEntity<List<DelegadoDto>> tablaDelegadosGeneral(@PathVariable(value = "idMesa") Long idMesa,
-    //         HttpServletRequest request) {
+    // public ResponseEntity<List<DelegadoDto>>
+    // tablaDelegadosGeneral(@PathVariable(value = "idMesa") Long idMesa,
+    // HttpServletRequest request) {
 
-    //     // System.out.println("AAAAAAAAAAAAAAAAAAA");
-    //     List<Object[]> delegados = habilitadoService.listarDelegadosPorMesa(idMesa);
-    //     List<DelegadoDto> delegadosDto = delegados.stream()
-    //             .map(arr -> new DelegadoDto(
-    //                     (String) arr[0], // nombre_facultad
-    //                     (String) arr[1], // nombre_carrera
-    //                     (String) arr[2], // ru_rd
-    //                     (String) arr[3], // tipo
-    //                     (String) arr[4], // apellidos
-    //                     (Long) arr[5], // id_persona
-    //                     (String) arr[6], // nombre_mesa
-    //                     (String) arr[7], // nombre_tipo_delegado
-    //                     (Long) arr[8] // id_votante_habilitado
-    //             ))
-    //             .collect(Collectors.toList());
+    // // System.out.println("AAAAAAAAAAAAAAAAAAA");
+    // List<Object[]> delegados = habilitadoService.listarDelegadosPorMesa(idMesa);
+    // List<DelegadoDto> delegadosDto = delegados.stream()
+    // .map(arr -> new DelegadoDto(
+    // (String) arr[0], // nombre_facultad
+    // (String) arr[1], // nombre_carrera
+    // (String) arr[2], // ru_rd
+    // (String) arr[3], // tipo
+    // (String) arr[4], // apellidos
+    // (Long) arr[5], // id_persona
+    // (String) arr[6], // nombre_mesa
+    // (String) arr[7], // nombre_tipo_delegado
+    // (Long) arr[8] // id_votante_habilitado
+    // ))
+    // .collect(Collectors.toList());
 
-    //     return ResponseEntity.ok(delegadosDto);
+    // return ResponseEntity.ok(delegadosDto);
     // }
 
     @GetMapping("/tablaDelegadosGeneral/{idMesa}")
@@ -160,5 +178,114 @@ public class delegadoController {
 
         model.addAttribute("delegados", delegadosDto);
         return "Delegado/tabla_registrosMesas";
+    }
+
+    @GetMapping("/generarExcelConsultasMesasGeneral")
+    public ResponseEntity<InputStreamResource> generarExcelSolicitudBecaR() throws IOException {
+
+        List<Mesa> listaMesas = mesaService.listarMesasOrdenadas();
+
+        // Crear un nuevo libro de Excel
+        Workbook workbook = new XSSFWorkbook();
+
+        Sheet sheet = workbook.createSheet("CONSULTA DE MESAS EN GENERAL");
+
+        // Agregar una fila adicional para la descripción
+        Row tituloGeneral = sheet.createRow(0);
+        Cell tituloGeneralCell = tituloGeneral.createCell(0);
+        tituloGeneralCell.setCellValue("CONSULTA DE MESAS EN GENERAL");
+
+        // Estilo de celda para texto en negrita y centrado
+        Font boldFont = sheet.getWorkbook().createFont();
+        boldFont.setBold(true);
+        CellStyle boldCenterCellStyle = sheet.getWorkbook().createCellStyle();
+        boldCenterCellStyle.setFont(boldFont);
+        boldCenterCellStyle.setAlignment(HorizontalAlignment.CENTER);
+
+        CellStyle boldCellStyle = sheet.getWorkbook().createCellStyle();
+        boldCellStyle.setFont(boldFont);
+        boldCellStyle.setAlignment(HorizontalAlignment.LEFT);
+
+        // Aplicar estilo a la celda de descripción
+        tituloGeneralCell.setCellStyle(boldCenterCellStyle);
+
+        int rowNum = 1;
+
+        String[] headers = { "#", "APELLIDOS Y NOMBRES", "TIPO PERSONA", "RU o RD", "FACULTAD", "CARRERA", "MESA",
+                "TIPO" };
+        sheet.addMergedRegion(new CellRangeAddress(0, 0, 0, headers.length - 1));
+
+        for (int i = 0; i < listaMesas.size(); i++) {
+            Row tituloMesa = sheet.createRow(rowNum++);
+            Cell cell = tituloMesa.createCell(0);
+            cell.setCellStyle(boldCellStyle);
+            cell.setCellValue(listaMesas.get(i).getNombre_mesa());
+
+            Row ubicacionMesa = sheet.createRow(rowNum++);
+            Cell cell2 = ubicacionMesa.createCell(0);
+            cell2.setCellValue(
+                    "UBICACIÓN: " + listaMesas.get(i).getUbicacion() + " - " + listaMesas.get(i).getDescripcion());
+
+            Row headerRow = sheet.createRow(rowNum++);
+            for (int j = 0; j < headers.length; j++) {
+                Cell cellHeader = headerRow.createCell(j);
+                cellHeader.setCellValue(headers[j]);
+            }
+
+            List<Object[]> delegados = habilitadoService.listarDelegadosPorMesa(listaMesas.get(i).getId_mesa());
+            List<DelegadoDto> listaDelegadosDto = delegados.stream()
+                    .map(arr -> new DelegadoDto(
+                            (String) arr[0], // nombre_facultad
+                            (String) arr[1], // nombre_carrera
+                            (String) arr[2], // ru_rd
+                            (String) arr[3], // tipo
+                            (String) arr[4], // apellidos
+                            (Long) arr[5], // id_persona
+                            (String) arr[6], // nombre_mesa
+                            (String) arr[7], // nombre_tipo_delegado
+                            (Long) arr[8] // id_votante_habilitado
+                    ))
+                    .collect(Collectors.toList());
+
+            for (int j = 0; j < listaDelegadosDto.size(); j++) {
+                Row row = sheet.createRow(rowNum++);
+                row.createCell(0).setCellValue(j + 1);
+                row.createCell(1).setCellValue(listaDelegadosDto.get(j).getApellidos());
+                row.createCell(2).setCellValue(listaDelegadosDto.get(j).getTipo_persona());
+                row.createCell(3).setCellValue(listaDelegadosDto.get(j).getRu_rd());
+                row.createCell(4).setCellValue(listaDelegadosDto.get(j).getNombre_facultad());
+                row.createCell(5).setCellValue(listaDelegadosDto.get(j).getNombre_carrera());
+                row.createCell(6).setCellValue(listaDelegadosDto.get(j).getNombre_mesa());
+                row.createCell(7).setCellValue(listaDelegadosDto.get(j).getTipo_persona());
+            }
+
+            Row saltoLinea = sheet.createRow(rowNum++);
+            Cell cellsaltoLinea = saltoLinea.createCell(0);
+            cellsaltoLinea.setCellValue("");
+        }
+
+        // for (int i = 0; i < headers.length; i++) {
+        // sheet.autoSizeColumn(i);
+        // }
+
+        // Escribir el libro de Excel en un flujo de bytes
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        workbook.write(outputStream);
+        workbook.close();
+
+        // Crear un InputStreamResource para el flujo de bytes
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+        InputStreamResource resource = new InputStreamResource(inputStream);
+
+        // Configurar los encabezados de respuesta para la descarga del archivo
+        HttpHeaders headersRespuesta = new HttpHeaders();
+        headersRespuesta.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=Mesas en General.xlsx");
+
+        // Devolver la respuesta con el StreamResource y los encabezados
+        return ResponseEntity.ok()
+                .headers(headersRespuesta)
+                .contentType(
+                        MediaType.parseMediaType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"))
+                .body(resource);
     }
 }
